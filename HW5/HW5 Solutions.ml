a70d56d9a0e9338b26acf391c0cc6d25b997dee4
amanij <amani.jammoul@mail.mcgill.ca>
2020-10-28 00:47:23 -0700

    complete find_path!

--- HW5/HW5 Solutions.ml
+++ HW5/HW5 Solutions.ml
@@ -34,26 +34,26 @@ let neighbours (g: 'a graph) (vertex: 'a) : ('a * weight) list =
 
 (* TODO: Implement find_path. *)
 let find_path (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
-  let rec aux_node (node: 'a) visited: 'a list =
-    if (List.exists (fun s->s=node) visited) then []
-    else visited@[node]
-  and aux_list (nodes:'a list) visited =
-    raise NotImplemented 
-  in
-  (*let try_path = ref [] in
-   let try_weight = ref 0 in*)
+  let rec aux_node (node: 'a) (visited: 'a list) : bool =
+    List.exists (fun s->s=node) visited(*match visited with
+      | [] -> false
+      | x::xs -> (if x = node then true else aux_node node xs)*)
+  in 
   let m = g.edges in
-  let rec find_inner_path (edges: ('a*'a*weight) list) (a': 'a) : ('a list * weight) =
+  let rec find_inner_path (edges: ('a*'a*weight) list) (a_inner : 'a) (visited: 'a list) (weight_acc: weight): ('a list * weight) =
     match edges with
     | []-> raise Fail
-    | (v1, v2, w)::[] -> if (v1=a') && (v2=b) then (!try_path@[b], !try_weight+w)
+    | (v1, v2, w)::[] -> if (v1=a_inner) && (v2=b) then (visited@[v2], weight_acc+w)
         else raise Fail
-    | (v1, v2, w)::xs ->if ((v1=a) && (List.exists (fun s->s=v2) !try_path) = false) then 
-          try (try_path := !try_path@[v2]; try_weight := !try_weight+w;
-               find_inner_path m v2) with
-            Fail-> find_inner_path xs a' 
-        else find_inner_path xs a 
-  in find_inner_path m a 
+    | (v1, v2, w)::xs -> if v1=a_inner then 
+          if v2 = b then (visited@[v2], weight_acc+w)
+          else
+          if (aux_node v2 visited) then find_inner_path xs a_inner visited weight_acc
+          else
+            try (find_inner_path m v2 (visited@[v2]) (weight_acc+w)) with
+              Fail-> (find_inner_path xs a_inner visited weight_acc ) 
+        else find_inner_path xs a_inner visited weight_acc
+  in find_inner_path m a [a] 0
 
 (* TODO: Implement find_path'. *)
 let find_path' (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
