c3de428eeffddaaf97e35f3113a2ae6ab0d7a82b
amanij <amani.jammoul@mail.mcgill.ca>
2020-10-30 00:31:28 -0700

    Q3 done & Q4 kind working

--- HW5/HW5 Solutions.ml
+++ HW5/HW5 Solutions.ml
@@ -34,47 +34,91 @@ let neighbours (g: 'a graph) (vertex: 'a) : ('a * weight) list =
 
 (* TODO: Implement find_path. *)
 let find_path (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
-  let rec aux_node (node: 'a) (visited: 'a list) : bool =
-    List.exists (fun s->s=node) visited(*match visited with
-      | [] -> false
-      | x::xs -> (if x = node then true else aux_node node xs)*)
+  let check (node: 'a) (visited: 'a list) : bool =
+    List.exists (fun s->s=node) visited
   in 
   let m = g.edges in
-  let rec find_inner_path (edges: ('a*'a*weight) list) (a_inner : 'a) (visited: 'a list) (weight_acc: weight): ('a list * weight) =
+  let rec find_inner_path (edges: ('a*'a*weight) list) (a_inner : 'a) 
+      (visited: 'a list) (weight_acc: weight): ('a list * weight) =
     match edges with
-    | []-> raise Fail
-    | (v1, v2, w)::[] -> if (v1=a_inner) && (v2=b) then (visited@[v2], weight_acc+w)
-        else raise Fail
+    | [] -> raise Fail
     | (v1, v2, w)::xs -> if v1=a_inner then 
           if v2 = b then (visited@[v2], weight_acc+w)
           else
-          if (aux_node v2 visited) then find_inner_path xs a_inner visited weight_acc
+          if (check v2 visited) then find_inner_path xs a_inner visited weight_acc
           else
             try (find_inner_path m v2 (visited@[v2]) (weight_acc+w)) with
-              Fail-> (find_inner_path xs a_inner visited weight_acc ) 
+              Fail-> (find_inner_path xs a_inner visited weight_acc) 
         else find_inner_path xs a_inner visited weight_acc
   in find_inner_path m a [a] 0
 
+
 (* TODO: Implement find_path'. *)
 let find_path' (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) =
-  let rec aux_node node visited fc sc =
-    raise NotImplemented
-  and aux_list nodes visited fc sc =
-    raise NotImplemented
-  in
-  raise NotImplemented
+  let check node visited =
+    List.exists (fun s->s=node) visited
+  in 
+  let m = g.edges in
+  let rec find_inner_path' edges a_inner visited weight_acc fc sc : ('a list * weight) = 
+    match edges with
+    | [] -> fc visited weight_acc
+    | (v1, v2, w)::xs -> if v1=a_inner then
+          if v2 = b then sc visited v2 weight_acc w
+          else
+          if (check v2 visited) then find_inner_path' xs a_inner visited weight_acc
+              fc sc
+          else find_inner_path' m v2 (visited@[v2]) (weight_acc+w)
+              (fun visited_list weigtht ->
+                 find_inner_path' xs a_inner visited weight_acc fc sc) sc
+        else find_inner_path' xs a_inner visited weight_acc fc sc 
+  in find_inner_path' m a [a] 0 
+    (fun visited_list weight -> raise Fail) 
+    (fun visited_list lastNode weight_acc 
+      lastWeight -> (visited_list@[lastNode], weight_acc+lastWeight))
+    
+let g1 = {nodes = ["a"; "b"; "c"; "d"; "e"];
+          edges = [("a", "b", 1); ("b", "a", 1); ("a", "c", 1); ("d", "a", 1);
+                   ("e", "d", 1); ("a", "e", 1); ("c", "d", 1)] } ;; 
 
-
-(* TODO: Implement find_all_paths *)
-let find_all_paths (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) list = 
-  let rec aux_node node visited =
-    raise NotImplemented  
-  and aux_list nodes visited = 
-    raise NotImplemented  
+(* TODO: Implement find_all_paths *) 
+let find_all_paths (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) list =
+  let check node visited = List.exists (fun s->s=node) visited in 
+  let m = g.edges in 
+  let rec find_one edges a_inner visited weight_acc: ('a list * weight) =
+    match edges with
+    | [] -> raise Fail
+    | (v1, v2, w)::xs -> if v1=a_inner then 
+          if v2 = b then (visited@[v2], weight_acc+w)
+          else if (check v2 visited) then find_one xs a_inner visited weight_acc
+          else try (find_one m v2 (visited@[v2]) (weight_acc+w)) with
+              Fail-> (find_one xs a_inner visited weight_acc) 
+        else find_one xs a_inner visited weight_acc
   in
-  raise NotImplemented  
+  let rec find_set edges a_inner weight_acc paths: ('a list * weight) list = 
+    match edges with
+    | [] -> [] 
+    | (v1, v2, w)::xs -> if (v1=a_inner) then
+          try (let next = find_one edges a_inner [a;a_inner] (weight_acc) in
+               if check next paths 
+               then find_set xs a_inner weight_acc paths
+               else ((find_one edges a_inner [a;a_inner] (weight_acc))::
+                     (find_set xs a_inner weight_acc (next::paths)))) with 
+            Fail -> []
+        else find_set xs a_inner weight_acc paths 
+  and find_all' edges : ('a list * weight) list =
+    match edges with
+    | [] -> []
+    | (v1, v2, w)::xs -> if v1 = a then 
+          if v2 = b then [([v1;v2], w)]@(find_all' xs)
+          else (find_set m v2 w [])@(find_all' xs)
+        else find_all' xs 
+  in find_all' m ;;
 
 
 (* TODO: Implement find_shortest_path *)
 let find_shortest_path (g: 'a graph) (a: 'a) (b: 'a) : ('a list * weight) option = 
-  raise NotImplemented    
+  raise NotImplemented
+    (*let all = find_all_paths g a b in
+   match all with
+   | [] -> None
+   | (a, w)::xs -> *)
